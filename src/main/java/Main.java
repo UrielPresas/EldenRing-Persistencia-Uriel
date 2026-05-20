7f207999da040cdd94ce57fc7d2df35db93d8053
UrielPresas <u.presas@sapalomera.cat>
2026-05-20 12:13:12 +0200

    main modified, removed extra lines of code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,6 @@
 import API.AshImporter;
 import API.BossImporter;
-import API.EldenRingApiClient;
-import API.DTO.WeaponResponse;
 import API.WeaponImporter;
-import com.google.gson.Gson;
 import DAO.Connexions.ConexioFactory;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -12,7 +9,7 @@ import java.util.Scanner;
 //import Vista.Vista;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
@@ -20,6 +17,12 @@ public class Main {
         String opcio = sc.nextLine();
 
         Connection conn = ConexioFactory.getConnection(opcio);
+
+        if (conn == null) {
+            System.out.println("Error en la connexió");
+            return;
+        }
+
         try {
             conn.setAutoCommit(false);
 
@@ -30,21 +33,22 @@ public class Main {
             conn.commit();
 
             System.out.println("Importació completada");
+
         } catch (Exception e) {
-            conn.rollback();
+            try {
+                conn.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+
             System.out.println("Rollback executat");
             e.printStackTrace();
-        }
-
-        if(conn != null){
-            System.out.println("Connexió correcta");
-            String json = EldenRingApiClient.getWeaponsJson();
-            Gson gson = new Gson();
-            WeaponResponse response =
-                    gson.fromJson(json, WeaponResponse.class);
-
-        } else {
-            System.out.println("Error en la connexió");
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
